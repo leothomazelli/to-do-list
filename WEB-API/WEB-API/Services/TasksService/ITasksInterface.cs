a3a4acd11da1fde390857853fb8ddb1ee3624e78
leonardo <leonardothomazellif@gmail.com>
2024-07-11 16:14:43 -0300

    initial service and interface setup

---
+++ WEB-API/WEB-API/Services/TasksService/ITasksInterface.cs
@@ -0,0 +1,13 @@
+﻿using WEB_API.Models;
+
+namespace WEB_API.Services.TasksService
+{
+    public interface ITasksInterface
+    {
+        Task<List<TasksModel>> CreateTask(TasksModel newTask);
+        Task<List<TasksModel>> UpdateTask(TasksModel editTask);
+        Task<List<TasksModel>> DeleteTaskById(int id);
+        Task<List<TasksModel>> GetTasks();
+        Task<TasksModel> GetTaskById(int id);
+    }
+}
---
+++ WEB-API/WEB-API/Services/TasksService/ITasksService.cs
@@ -0,0 +1,31 @@
+﻿using WEB_API.Models;
+
+namespace WEB_API.Services.TasksService
+{
+    public class ITasksService : ITasksInterface
+    {
+        public Task<List<TasksModel>> CreateTask(TasksModel newTask)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<TasksModel>> UpdateTask(TasksModel editTask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TasksModel>> DeleteTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TasksModel>> GetTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TasksModel> GetTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
